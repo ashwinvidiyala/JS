92d4b918ed5db2b4ad9d72a14297cb2375b0b375
“Ashwin <ashwinvidiyala@gmail.com>
2018-03-16 13:57:47 -0500

    Finish Restful Tasks Continued Assignment

--- Angular/task_api_angular/client/src/app/app.component.ts
+++ Angular/task_api_angular/client/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component }    from '@angular/core';
-import { HttpService }  from './http.service';
+import { Component, OnInit }    from '@angular/core';
+import { HttpService }          from './http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+  tasks = [];
   constructor(private _httpService: HttpService) {}
+
+  ngOnInit() {
+    this.getTasksFromService();
+  }
+
+  getTasksFromService() {
+    let observable = this._httpService.getTasks();
+    observable.subscribe( (data) => {
+      console.log('Got our tasks!!', data.json());
+      this.tasks = data.json().tasks;
+    }
+  }
 }
--- Angular/task_api_angular/client/src/app/http.service.ts
+++ Angular/task_api_angular/client/src/app/http.service.ts
@@ -5,20 +5,22 @@ import { Http }        from '@angular/http';
 export class HttpService {
 
   constructor(private _http: Http) {
-    this.getTasks();
+    // this.getTasks();
   }
 
   getTasks() {
-    let tempObservable = this._http.get('/tasks');
-    tempObservable.subscribe( (data) => {
-      let tasks_array = data.json().tasks;
-      for (let i = 0; i < tasks_array.length; i++) {
-        let task_id = tasks_array[i]._id;
-        let taskObservable = this._http.get(`/tasks/${task_id}`);
-        taskObservable.subscribe( (task) => {
-          console.log(`Title #${i+1}: ${task.json().data.title}`);
-        });
-      }
-    });
-  };
+    return this._http.get('/tasks');
+
+    // let tempObservable = this._http.get('/tasks');
+    // tempObservable.subscribe( (data) => {
+    //   let tasks_array = data.json().tasks;
+    //   for (let i = 0; i < tasks_array.length; i++) {
+    //     let task_id = tasks_array[i]._id;
+    //     let taskObservable = this._http.get(`/tasks/${task_id}`);
+    //     taskObservable.subscribe( (task) => {
+    //       console.log(`Title #${i+1}: ${task.json().data.title}`);
+    //     })
+    //   }
+    // })
+  }
 }
